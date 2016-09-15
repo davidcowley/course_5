cea9be06fb38a86cfddc553ef4b0618ad6444e51
davidcowley <davidcowley@4fast.net>
2016-09-15 14:45:05 -0700

    assignment 1 folder

---
+++ mod1_solution/javascripts/app.js
@@ -0,0 +1,49 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheckApp', [])
+.controller('LunchCheckController', LunchCheckController);
+
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.menu_items = "";
+  $scope.menu_msg = "";
+
+  $scope.menu_btn = function () {
+
+    if ($scope.menu_items.trim().length == 0) {
+       $scope.menu_msg = "Please enter data first";
+       $scope.customStyle.colorClass = "red";
+    } else  {
+       var number_of_items = splitString($scope.menu_items, ",");
+       if (number_of_items <= 3) {
+          $scope.menu_msg = "Enjoy!";
+          $scope.customStyle.colorClass = "green";
+          //turnGreen;
+       } else {
+          $scope.menu_msg = "Too Many!";
+          $scope.customStyle.colorClass = "green";
+       }
+    }
+    $scope.menu_items = "";
+};
+
+$scope.customStyle = {};
+$scope.turnGreen = function (){
+   $scope.customStyle.colorClass = "green";
+}
+
+$scope.turnBlue = function() {
+   $scope.customStyle.colorClass = "blue";
+}
+
+}
+
+function splitString(stringToSplit, separator) {
+  var arrayOfStrings = stringToSplit.split(separator);
+  return arrayOfStrings.length;
+}
+
+
+})();
