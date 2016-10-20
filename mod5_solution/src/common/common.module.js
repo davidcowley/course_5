009b3f8c3bccfcff748f5b7f4d03c34e57fccb89
davidcowley <davidcowley@4fast.net>
2016-10-20 11:35:02 -0700

    https vs http

--- mod5_solution/src/common/common.module.js
+++ mod5_solution/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'http://dcowley-cs5.herokuapp.com/')
+.constant('ApiPath', 'https://dcowley-cs5.herokuapp.com/')
 .config(config);
 
 config.$inject = ['$httpProvider'];
